512f378419af35f90949be04917297b425acf0b0
Rafael Marinho <rafaelmarinho.dev@gmail.com>
2025-02-25 16:59:50 -0300

    feat: #202502251659 initial commit

---
+++ src/app/components/sale-items/sale-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sale-items',
+  imports: [],
+  templateUrl: './sale-items.component.html',
+  styleUrl: './sale-items.component.css'
+})
+export class SaleItemsComponent {
+
+}
